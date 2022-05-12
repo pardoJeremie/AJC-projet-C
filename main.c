43fb003c8be02393b5d563f8201f3c59ac2c7f2b
Jérémie PARDO <Pardo_Jeremie@outlook.com>
2022-05-12 11:16:13 +0200

    Adding main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AJC projet C
+//
+//  Created by pardo jérémie on 12/05/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
