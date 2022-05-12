73c8ffe0d4d5e398146787c162b767940a1758fd
Jérémie PARDO <Pardo_Jeremie@outlook.com>
2022-05-12 11:46:40 +0200

    Adding of the skeleton of the program functions and files

---
+++ function.c
@@ -0,0 +1,31 @@
+//
+//  function.c
+//  AJC projet C
+//
+//  Created by pardo jérémie on 12/05/2022.
+//
+
+#include "function.h"
+
+void encrypt_file (char*);
+void decryt_to_printf (void);
+void decryt_to_file (char*);
+                   
+void menu (void) {
+}
+
+void encrypt_file (char* file_to_encrypt) {
+    
+}
+
+void decryt_to_printf (void) {
+    
+}
+
+void decryt_to_file (char* file_to_decrypt) {
+    
+}
+
+void change_parrot_phrase (void) {
+    
+}
--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include "function.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
